07dafc1c67931dd88e6c28b3e3fb6e3f2ed6e949
Weslley Araujo <weslleyaraujo20@gmail.com>
2015-08-15 14:04:20 -0300

    [WIP] Starting sass task

---
+++ gulp-tasks/sass.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var gulp = require('gulp');
+var plugins = require('gulp-load-plugins')();
+var options = require('../_shared/options.js');
+
+module.exports = function() {
+  gulp.task('sass', function() {
+    gulp.src('_sites/' + options.project + '/src/assets/sass/*.sass')
+      .pipe(plugins.sourcemaps.init())
+      .pipe(plugins.sass().on('error', function (err) {
+        plugins.notify().write({
+          title: 'Error compiling sass.',
+          message: err.message
+        });
+      }))
+      .pipe(plugins.sourcemaps.write())
+      .pipe(gulp.dest('_sites/' + options.project + '/dist/css'));
+  });
+};
