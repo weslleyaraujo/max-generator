1fd4421186597a7e5d7c2e87541e6d8387de12da
Weslley Araujo <weslleyaraujo20@gmail.com>
2015-08-18 00:24:33 -0300

    [WIP] 😓

--- _shared/get-site-data.js
+++ _shared/get-site-data.js
@@ -2,21 +2,28 @@ var recursive = require('recursive-readdir');
 var options = require('../_shared/options.js');
 var path = options.src.projectRoot + options.src.data;
 
+function getName(filename) {
+  return filename.replace(/.*\/data\//g, '');
+}
 function getJSON(filename) {
-  var name = filename.replace(/.*\/data\//g, '');
-
-  if (name.match(/\//)) {
-    console.log(name);
-  }
-
+  var name = getName(filename);
   return require(filename);
 }
 
 recursive(path, function(err, files) {
-  var result = files.reduce(function (curr, filename) {
-    curr[filename] = getJSON(filename);
-    return curr;
+  var regex = new RegExp(/\//);
+
+  files = files.reduce(function (current, item) {
+    var name = getName(item);
+    current[name] = current[name] || {};
+
+    if (regex.test(name)) {
+
+    }
+
+    return current;
+
   }, {});
 
-  console.log(result);
+  console.log(files);
 });
